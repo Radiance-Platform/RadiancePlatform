10b829578150d77f82542e17bccf7082fd9e32e5
hbh7 <hbh7@hbh7.com>
2022-03-11 21:35:58 -0500

    Example tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,29 @@
+// Can be run with `cargo test`
+
+// Example tests to base our own off of later
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(bad_add(1, 2), 3);
+    }
+
+    // Lets us use ? to return quicker when failing
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
+    }
+}
