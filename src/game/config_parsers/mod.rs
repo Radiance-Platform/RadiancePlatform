93322cbb78ee39fa93027666745b399301abbe9e
hbh7 <hbh7@hbh7.com>
2022-03-17 13:42:13 -0400

    Scan config work

--- src/game/config_parsers/mod.rs
+++ src/game/config_parsers/mod.rs
@@ -1,3 +1,6 @@
+use std::path::{Component, Path};
+use walkdir::WalkDir;
+
 mod characters;
 mod game;
 mod maps;
@@ -7,12 +10,24 @@ pub struct GameData {
     maps: u8,
 }
 
-fn scan_config() {
-
+fn scan_config(config_path:std::path::PathBuf) {
+    // Loop over every file in the provided folder
+    for entry in WalkDir::new(config_path) {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+        let components = entry.path().components().collect::<Vec<_>>();
+        println!("{:?}", components);
+/*        match components[1] {
+            _ => { println!("{}", _)}
+        }*/
+    }
 }
 
 pub fn process_configs(config_path:std::path::PathBuf) -> GameData {
     println!("Parsing configs");
+
+    scan_config(config_path);
+
     GameData {
         maps: 1,
     }
