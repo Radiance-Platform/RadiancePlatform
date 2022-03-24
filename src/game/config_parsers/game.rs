034e1d2a21c523a8cb1e78e75db717e229a7414f
Ella Coleman <coleme@rpi.edu>
2022-03-24 16:57:23 -0400

    Finished parsing GameInfo

--- src/game/config_parsers/game.rs
+++ src/game/config_parsers/game.rs
@@ -15,22 +15,47 @@ pub fn process_config(game_data: &mut GameData, config_path: &Path) -> Result<()
     let doc = &docs[0];
 
     // Debug print
-    println!("GAME DOC: {:?}\n", doc);
+    //println!("GAME DOC: {:?}\n", doc);
 
-    let game_data_hash = doc.as_hash().unwrap();
+    let game_info_hash = doc.as_hash().unwrap();
 
-    let key = yaml_rust::Yaml::from_str("name");
-    let name = game_data_hash.get(&key).unwrap_or(&key).as_str().unwrap();
+    for key in game_info_hash.keys() {
+        let val = game_info_hash.get(&key).unwrap_or(&key);
+        let key_str = key.as_str().unwrap();
+        match key_str {
+            "name" => {
+                game_data.info.name = val.as_str().unwrap().to_string();
+            }
+            "description" => {
+                game_data.info.description = val.as_str().unwrap().to_string();
+            }
+            "author" => {
+                game_data.info.author = val.as_str().unwrap().to_string();}
+            "min_screen_size" => {
+                let width_key = yaml_rust::Yaml::from_str("width");
+                let width = val.as_hash().unwrap()
+                                    .get(&width_key).unwrap_or(&key)
+                                    .as_i64().unwrap();
+                let height_key = yaml_rust::Yaml::from_str("height");
+                let height = val.as_hash().unwrap()
+                                    .get(&height_key).unwrap_or(&key)
+                                    .as_i64().unwrap();
 
-    println!("GAME NAME: {:?}\n", name);
-
-    // Todo: Parse fields
-    game_data.info.name = name.to_string();
-    game_data.info.author = "The Radiance Team".to_string();
-    // ...
+                game_data.info.min_screen_cols = width as u16;
+                game_data.info.min_screen_rows = height as u16;
+            }
+            _ => {continue;}
+        }
+    }
+    /* Debug */
+    println!();
+    println!("Debug config_parsers/game.rs:");
+    println!("game_data.info.name = {:?}", game_data.info.name);
+    println!("game_data.info.description = {:?}", game_data.info.description);
+    println!("game_data.info.author = {:?}", game_data.info.author);
+    println!("game_data.info.min_screen_cols = {:?}", game_data.info.min_screen_cols);
+    println!("game_data.info.min_screen_rows = {:?}", game_data.info.min_screen_rows);
+    println!();
 
     Ok(())
 }
-
-/* TO DO: Add function that takes the name of the yaml field and returns the data
-   associated with it completely unpacked as a string */
