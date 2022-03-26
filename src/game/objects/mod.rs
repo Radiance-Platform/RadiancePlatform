92c45b060f6bfe15ad737bb87ac8185a5b806bd0
hbh7 <hbh7@hbh7.com>
2022-03-26 00:06:27 -0400

    Misc struct restructuring

--- src/game/objects/mod.rs
+++ src/game/objects/mod.rs
@@ -2,19 +2,19 @@
 
 pub struct ObjectState {
     pub name: String,
-    pub values: Vec<String>
+    pub values: String
 }
 
-pub struct ObjectInteractions {
+pub struct ObjectInteraction {
     pub name: String,
-    pub values: Vec<String>
+    pub values: String
 }
 
 pub struct Object {
     pub name: String,
     pub icon: char,
     pub state: Vec<ObjectState>,
-    pub interactions: Vec<ObjectInteractions>,
+    pub interactions: Vec<ObjectInteraction>,
 }
 
 impl Object {
