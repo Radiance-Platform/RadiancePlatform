d3a9c9df8a0aaa720c95a91f2ed01d5aacbc8bd6
YoshikoJanai <39793714+yoshikojanai@users.noreply.github.com>
2022-04-08 23:05:51 -0400

    Refactor Config parser stuff

--- src/game/characters/mod.rs
+++ src/game/characters/mod.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize,Deserialize};
+
 use crate::game::characters::attribute::Attribute;
 use crate::game::characters::role::Role;
 use crate::game::objects::Object;
@@ -29,45 +29,3 @@ impl Character {
 
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CharacterData {
-    pub id: String,
-    pub name: String,
-    pub icon: char,
-    pub inventory_size: InventorySize,
-    pub traits: Vec<Trait>,
-    pub interactions: Interactions,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct InventorySize {
-    pub width: i64,
-    pub height: i64,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Trait {
-    pub name: String,
-    pub display_name: String,
-    pub starting_value: u8,
-    pub max_value: u8,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Interactions {
-    pub attacks: Vec<Attack>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Attack {
-    pub name: String,
-    pub display_name: String,
-    pub base_damage: i64,
-    pub affected_by: Vec<AffectedBy>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AffectedBy {
-    pub skill: String,
-    pub effect_per_point: String,
-}
--- src/game/config_parsers/characters.rs
+++ src/game/config_parsers/characters.rs
@@ -3,7 +3,8 @@ use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 use yaml_rust::YamlLoader;
-use crate::game::characters::{Character, CharacterData};
+use serde::{Serialize,Deserialize};
+use crate::game::characters::{Character};
 use crate::game::characters::role::Role;
 use crate::game::config_parsers::GameData;
 use crate::game::characters::attribute::Attribute;
@@ -50,4 +51,47 @@ fn get_character_from_data(characters: &mut HashMap<String, Character>, data: Ch
         character.attributes.push(attribute);
     }
     characters.insert(character.name.clone(), character);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CharacterData {
+    pub id: String,
+    pub name: String,
+    pub icon: char,
+    pub inventory_size: InventorySize,
+    pub traits: Vec<Trait>,
+    pub interactions: Interactions,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InventorySize {
+    pub width: i64,
+    pub height: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Trait {
+    pub name: String,
+    pub display_name: String,
+    pub starting_value: u8,
+    pub max_value: u8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Interactions {
+    pub attacks: Vec<Attack>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Attack {
+    pub name: String,
+    pub display_name: String,
+    pub base_damage: i64,
+    pub affected_by: Vec<AffectedBy>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AffectedBy {
+    pub skill: String,
+    pub effect_per_point: String,
 }
